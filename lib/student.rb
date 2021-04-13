8b834c511610d14f373716d874183002ac1d16ec
Sam D <samidiaz3@gmail.com>
2021-04-13 16:17:00 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,27 +1,26 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
   def initialize(student_hash)
-    
+
   end
 
   def self.create_from_collection(students_array)
     # iterates over the array of hashes and creates a new indiv student using each hash
     students_array.each do |student|
-      
-      
+
+
     end
   end
 
   def add_student_attributes(attributes_hash)
-    
+
   end
 
   def self.all
-    
+
   end
 end
-
