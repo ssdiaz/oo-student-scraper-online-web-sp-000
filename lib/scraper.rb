e9f21ba6d0c8f6ed98a48af39bd1e695ceb67d94
Sam D <samidiaz3@gmail.com>
2021-04-13 15:20:22 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -51,6 +51,11 @@ class Scraper
             {
               #if profile.css("div.social-icon-container a").attribute("href").text.include?("twitter")
                 :twitter => icon_section.css("a").attribute("href").text
+                :linkedin => icon_section.css("a").attribute("href").text
+                # :github =>
+                # :blog =>
+                # :profile_quote =>
+                # :bio =>
               #end
           }
         end
