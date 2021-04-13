ce97af4720bd060610d28e417b7404e7c2fa4324
Sam D <samidiaz3@gmail.com>
2021-04-13 16:16:58 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -9,7 +9,11 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    
+    # iterates over the array of hashes and creates a new indiv student using each hash
+    students_array.each do |student|
+      
+      
+    end
   end
 
   def add_student_attributes(attributes_hash)
