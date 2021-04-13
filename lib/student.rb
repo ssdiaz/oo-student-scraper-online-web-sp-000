e9acd584acc1af12094540ae06ba24d6da5efb7d
Sam D <samidiaz3@gmail.com>
2021-04-13 17:08:47 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -8,7 +8,7 @@ class Student
     student_hash.each do |key, value|
       self.send("#{key.to_s}=", value)
     end
-    @@all<<self
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
