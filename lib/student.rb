f6c28a9e88ff25a7005d7c12a83224123877018e
Sam D <samidiaz3@gmail.com>
2021-04-13 16:55:29 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -15,8 +15,9 @@ class Student
     # iterates over the array of hashes and creates a new indiv student using each hash
     students_array.each do |student|
     #  Student.new(nattributes)
+              binding.pry
     end
-          binding.pry
+
   end
 
   # [1] pry(Scraper)> students
