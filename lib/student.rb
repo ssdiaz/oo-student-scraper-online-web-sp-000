643fd19ace827b1be379fe0cada86f615a18f717
Sam D <samidiaz3@gmail.com>
2021-04-13 16:59:33 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,6 +18,19 @@ class Student
     end
   end
 
+  # [1] pry(#<Student>)> attributes_hash
+  # => {:twitter=>"someone@twitter.com",
+  #  :linkedin=>"someone@linkedin.com",
+  #  :github=>"someone@github.com",
+  #  :blog=>"someone@blog.com",
+  #  :profile_quote=>
+  #   "\"Forget safety. Live where you fear to live. Destroy your reputation. Be notorious.\" -
+  # Rumi",
+  #  :bio=>
+  #   "I was in southern California for college (sun and In-n-Out!), rural Oregon for high schoo
+  # l (lived in a town with 1500 people and 3000+ cows), and Tokyo for elementary/middle school.
+  # "}
+
   def add_student_attributes(attributes_hash)
     binding.pry
   end
