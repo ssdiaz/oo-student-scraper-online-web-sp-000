7dfe60e4179ccc2ea4e1942a2a1a1444e952a479
Sam D <samidiaz3@gmail.com>
2021-04-13 15:59:47 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -56,10 +56,10 @@ class Scraper
          end
          hash_socials[:bio] = profile.css("div.description-holder p").text
          hash_socials[:profile_quote] = profile.css("div.profile-quote").text
-         hash_socials[:blog] = 
+         hash_socials[:blog] =
        end
         binding.pry
-        
+
     #hash_socials
   end
 
