3564e0d7c8d0e3ac90310510f1965bfbdc2fbd42
Sam D <samidiaz3@gmail.com>
2021-04-13 17:08:08 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -7,8 +7,9 @@ class Student
   def initialize(student_hash)
     student_hash.each do |key, value|
       self.send("#{key.to_s}=", value)
-      @@all << self
+      #@@all << self
     end
+    @@all<<self
   end
 
   def self.create_from_collection(students_array)
