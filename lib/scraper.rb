0e7a8060e43a1efc70364b68647d75a7e9729f97
Sam D <samidiaz3@gmail.com>
2021-04-13 15:31:52 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -55,7 +55,7 @@ class Scraper
                 # :github =>
                 # :blog =>
                 # :profile_quote =>
-                # :bio =>
+                 :bio => profile.css("div.description-holder.p")
               #end
           }
         end
